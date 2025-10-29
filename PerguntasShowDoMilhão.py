9dbbd6fb4cb839e80f941d904b020c92f35d846b
YaGoSkktm <yagodezoito@gmail.com>
2025-10-29 14:30:24 -0300

    Jogos em Python

---
+++ PerguntasShowDoMilhão.py
@@ -0,0 +1,252 @@
+perguntasMilhao = [
+    {
+        "pergunta": "Qual é o maior planeta do Sistema Solar?",#1
+        "opcoes": ["1) Terra", "2) Júpiter", "3) Saturno", "4) Marte"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual é a capital da França?",#2
+        "opcoes": ["1) Roma", "2) Berlim", "3) Paris", "4) Londres"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Qual é o símbolo químico do ouro?",#3
+        "opcoes": ["1) Ag", "2) Au", "3) O", "4) Gd"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Quantos segundos há em uma hora?",#4
+        "opcoes": ["1) 3.000", "2) 3.600", "3) 6.000", "4) 60.000"],
+        'resposta': "2"
+    },
+    {
+        "pergunta": "Em que continente fica o Egito?",#5
+        "opcoes": ["1) África", "2) Ásia", "3) Europa", "4) América"],
+        "resposta": "1"
+    },
+    {
+        "pergunta": "Quem foi o primeiro presidente do Brasil?",#6
+        "opcoes": ["1) Getúlio Vargas", "2) Juscelino Kubitschek", "3) Marechal Deodoro da Fonseca", "4) Dom Pedro II"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Quem escreveu 'Dom Casmurro'?",#7
+        "opcoes": ["1) José de Alencar", "2) Machado de Assis", "3) Monteiro Lobato", "4) Jorge Amado"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Em que país nasceu o tango?",#8
+        "opcoes": ["1) Brasil", "2) Uruguai", "3) Argentina", "4) Chile"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Qual desses animais é um mamífero?",#9
+        "opcoes": ["1) Tubarão", "2) Golfinho", "3) Jacaré", "4) Pinguim"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Em que ano o homem pisou na Lua pela primeira vez?",#10
+        "opcoes": ["1) 1965", "2) 1969", "3) 1971", "4) 1975"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Quem foi o autor da frase 'Penso, logo existo'?",#11
+        "opcoes": ["1) Sócrates", "2) Aristóteles", "3) René Descartes", "4) Platão"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Quantos ossos tem o corpo humano adulto?",#12
+        "opcoes": ["1) 200", "2) 206", "3) 208", "4) 212"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Quem formulou as leis do movimento e da gravitação universal?",#13
+        "opcoes": ["1) Albert Einstein", "2) Isaac Newton", "3) Galileo Galilei", "4) Johannes Kepler"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual é o nome do criador da Microsoft?",#14
+        "opcoes": ["1) Steve Jobs", "2) Elon Musk", "3) Bill Gates", "4) Mark Zuckerberg"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Qual país tem o formato aproximado de uma bota?",#15
+        "opcoes": ["1) Espanha", "2) Itália", "3) Portugal", "4) Grécia"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual é o rio mais longo do mundo?",#16
+        "opcoes": ["1) Rio Nilo", "2) Rio Amazonas", "3) Rio Yangtzé", "4) Rio Mississippi"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual oceano banha a costa leste do Brasil?",#17
+        "opcoes": ["1) Oceano Pacífico", "2) Oceano Índico", "3) Oceano Ártico", "4) Oceano Atlântico"],
+        "resposta": "4"
+    },
+    {
+        "pergunta": "Quem pintou a 'Mona Lisa'?",#18
+        "opcoes": ["1) Vincent van Gogh", "2) Pablo Picasso", "3) Leonardo da Vinci", "4) Michelangelo"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Qual é a montanha mais alta do mundo?",#19
+        "opcoes": ["1) K2", "2) Monte Everest", "3) Kangchenjunga", "4) Lhotse"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual é o menor país do mundo?",#20
+        "opcoes": ["1) Mônaco", "2) Nauru", "3) Tuvalu", "4) Vaticano"],
+        "resposta": "4"
+    },
+    {
+        "pergunta": "Qual é a principal função do coração?",#21
+        "opcoes": ["1) Produzir hormônios", "2) Bombear sangue", "3) Filtrar o ar", "4) Digerir alimentos"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "De qual país é a invenção do chuveiro elétrico?",#22
+        "opcoes": ["1) Estados Unidos", "2) Alemanha", "3) Brasil", "4) Japão"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Qual é a nacionalidade de Che Guevara?",#23
+        "opcoes": ["1) Cubana", "2) Boliviana", "3) Argentina", "4) Peruana"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Qual o nome do processo de transformação da água do estado líquido para o gasoso?",#24
+        "opcoes": ["1) Condensação", "2) Solidificação", "3) Fusão", "4) Evaporação"],
+        "resposta": "4"
+    },
+    {
+        "pergunta": "Quantos continentes existem?",#25
+        "opcoes": ["1) 5", "2) 6", "3) 7", "4) 8"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual é o maior deserto do mundo?",#26
+        "opcoes": ["1) Saara", "2) Gobi", "3) Antártico", "4) Arábia"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Quem escreveu 'Cem Anos de Solidão'?",#27
+        "opcoes": ["1) Mario Vargas Llosa", "2) Julio Cortázar", "3) Jorge Luis Borges", "4) Gabriel García Márquez"],
+        "resposta": "4"
+    },
+    {
+        "pergunta": "Qual o livro mais vendido no mundo depois da Bíblia?",#28
+        "opcoes": ["1) O Senhor dos Anéis", "2) Dom Quixote", "3) O Pequeno Príncipe", "4) Um Conto de Duas Cidades"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual a primeira mulher a voar sozinha sobre o Oceano Atlântico?",#29
+        "opcoes": ["1) Amelia Earhart", "2) Valentina Tereshkova", "3) Sally Ride", "4) Harriet Quimby"],
+        "resposta": "1"
+    },
+    {
+        "pergunta": "Qual a fórmula química da água?",#30
+        "opcoes": ["1) CO2", "2) O2", "3) H2O", "4) NaCl"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Qual planeta é conhecido como 'Planeta Vermelho'?",#31
+        "opcoes": ["1) Vênus", "2) Marte", "3) Júpiter", "4) Saturno"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual é a moeda oficial do Japão?",#32
+        "opcoes": ["1) Yuan", "2) Won", "3) Iene", "4) Dólar"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Quantos jogadores um time de futebol de campo tem em campo?",#33
+        "opcoes": ["1) 9", "2) 10", "3) 11", "4) 12"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Qual a capital da Austrália?",#34
+        "opcoes": ["1) Sydney", "2) Melbourne", "3) Brisbane", "4) Camberra"],
+        "resposta": "4"
+    },
+    {
+        "pergunta": "Qual artista é famoso por ter cortado a própria orelha?",#35
+        "opcoes": ["1) Claude Monet", "2) Vincent van Gogh", "3) Pablo Picasso", "4) Salvador Dalí"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Em que ano o Muro de Berlim foi derrubado?",#36
+        "opcoes": ["1) 1985", "2) 1987", "3) 1989", "4) 1991"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Qual o principal ingrediente do guacamole?",#37
+        "opcoes": ["1) Tomate", "2) Abacate", "3) Pimentão", "4) Cebola"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual o maior mamífero do mundo?",#38
+        "opcoes": ["1) Elefante Africano", "2) Baleia Azul", "3) Girafa", "4) Rinoceronte Branco"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual instrumento musical clássico tem 88 teclas?",#39
+        "opcoes": ["1) Violino", "2) Harpa", "3) Piano", "4) Violoncelo"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Em que país os Jogos Olímpicos se originaram?",#40
+        "opcoes": ["1) Itália", "2) Egito", "3) Roma", "4) Grécia"],
+        "resposta": "4"
+    },
+    {
+        "pergunta": "Qual o animal terrestre mais rápido?",#41
+        "opcoes": ["1) Leopardo", "2) Leão", "3) Guepardo", "4) Cavalo"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Quem é a autora da saga 'Harry Potter'?",#42
+        "opcoes": ["1) Suzanne Collins", "2) J.K. Rowling", "3) Stephenie Meyer", "4) Veronica Roth"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Quais são as três cores primárias?",#43
+        "opcoes": ["1) Verde, Laranja e Roxo", "2) Branco, Preto e Cinza", "3) Vermelho, Amarelo e Azul", "4) Rosa, Marrom e Ciano"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Em qual cidade fica o Coliseu?",#44
+        "opcoes": ["1) Atenas", "2) Istambul", "3) Cairo", "4) Roma"],
+        "resposta": "4"
+    },
+    {
+        "pergunta": "Qual é o maior órgão do corpo humano?",#45
+        "opcoes": ["1) Cérebro", "2) Fígado", "3) Pele", "4) Intestino"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Quantos lados tem um hexágono?",#46
+        "opcoes": ["1) 5", "2) 6", "3) 7", "4) 8"],
+        "resposta": "2"
+    },
+    {
+        "pergunta": "Qual o nome do primeiro homem a viajar para o espaço?",#47
+        "opcoes": ["1) Neil Armstrong", "2) Buzz Aldrin", "3) Yuri Gagarin", "4) John Glenn"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Qual gás as plantas absorvem da atmosfera para fazer fotossíntese?",#48
+        "opcoes": ["1) Oxigênio", "2) Nitrogênio", "3) Dióxido de Carbono", "4) Hidrogênio"],
+        "resposta": "3"
+    },
+    {
+        "pergunta": "Quem compôs a famosa obra 'As Quatro Estações'?",#49
+        "opcoes": ["1) Mozart", "2) Beethoven", "3) Bach", "4) Vivaldi"],
+        "resposta": "4"
+    },
+    {
+        "pergunta": "Qual foi a primeira civilização a usar a escrita?",#50
+        "opcoes": ["1) Egípcios", "2) Gregos", "3) Sumérios", "4) Romanos"],
+        "resposta": "3"
+    }
+]
